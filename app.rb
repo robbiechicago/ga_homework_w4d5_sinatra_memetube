947d3143b35c5422392ab2a1cb1f1c3040d906f3
Rob Holloway <robbiechicago@gmail.com>
2015-09-06 15:48:52 +0100

    SHOW stage now working, with embedded vids appearing and playing.

--- app.rb
+++ app.rb
@@ -18,12 +18,18 @@ end
 
 # index
 get '/videos' do
-
+  sql = 'SELECT * FROM videos'
+  @vids = @db.exec(sql)
+  erb :index
 end
 
 # show
 get '/videos/:id' do
-
+  # binding.pry
+  sql = "SELECT * FROM videos WHERE id = #{params[:id]}"
+  @vidurl = @db.exec(sql).first
+  # WTF is that .first doing?  I know I need it but don't understand why.
+  erb :player
 end
 
 get '/videos/new' do
--- memetube.sql
+++ memetube.sql
@@ -12,12 +12,12 @@ create table videos (
   rating smallint check (rating > 0 and rating <6)
 );
 
-insert into videos (title, url, dscr) values ('Charlie Austin scores a cracking goal as Swindon beat Southampton 1-0 at St.Marys', 'https://youtu.be/OqE2vkYYNB8', 'Glad we moved seats as we were near the back in the first half ! Charlie takes his goal brilliantly to give Swindon a 1-0 win at Southampton. Also brought me 36 pounds from a 1 pound stake as I''d bet on Austin to score first and 1-0 Town. What a memorable night out in Southampton !');
+insert into videos (title, url, dscr) values ('Charlie Austin scores a cracking goal as Swindon beat Southampton 1-0 at St.Marys', 'OqE2vkYYNB8', 'Glad we moved seats as we were near the back in the first half ! Charlie takes his goal brilliantly to give Swindon a 1-0 win at Southampton. Also brought me 36 pounds from a 1 pound stake as I''d bet on Austin to score first and 1-0 Town. What a memorable night out in Southampton !');
 
-insert into videos (title, url, dscr) values ('Swindon Town score in the last minute to equalise against Brentford 3-3 in 2013 play-off','https://youtu.be/_SMIHbMLZ6A','Swindon come from 3-1 down and score the equaliser with virtually the last kick of normal time. One of the most euphoric celebrations I have EVER been lucky enough to be a part of!');
+insert into videos (title, url, dscr) values ('Swindon Town score in the last minute to equalise against Brentford 3-3 in 2013 play-off','SMIHbMLZ6A','Swindon come from 3-1 down and score the equaliser with virtually the last kick of normal time. One of the most euphoric celebrations I have EVER been lucky enough to be a part of!');
 
-insert into videos (title, url, dscr) values ('DANNY WARD','https://youtu.be/aMsARnMuHwk','Danny ward vs Charlton');
+insert into videos (title, url, dscr) values ('DANNY WARD','aMsARnMuHwk','Danny ward vs Charlton');
 
-insert into videos (title, url, dscr) values ('Swindon Town 1-0 Bristol City Michael Smith Goal','https://youtu.be/pL2TcjAe05U','Swindon Town 1-0 Bristol City. Smith winning goal!');
+insert into videos (title, url, dscr) values ('Swindon Town 1-0 Bristol City Michael Smith Goal','pL2TcjAe05U','Swindon Town 1-0 Bristol City. Smith winning goal!');
 
-insert into videos (title, url, dscr) values ('Nathan Byrne 90"+4 injury time winner - Sheffield Utd v Swindon Town, 1st Leg Play-Off Semi Finals','https://youtu.be/VXQlNMTBGVk','Nathan Byrne nets Swindon Town's last gasp winner away at Sheffield Utd to take a slender 2-1 advantage to the County Ground for the 2nd leg in the League One Play-Off Semi Finals with a place at Wembley at stake.');
+insert into videos (title, url, dscr) values ('Nathan Byrne 90"+4 injury time winner - Sheffield Utd v Swindon Town, 1st Leg Play-Off Semi Finals','VXQlNMTBGVk','Nathan Byrne nets Swindon Town''s last gasp winner away at Sheffield Utd to take a slender 2-1 advantage to the County Ground for the 2nd leg in the League One Play-Off Semi Finals with a place at Wembley at stake.');
